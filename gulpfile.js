3f84fbf71a2177acede937228cbeb2b8447ac586
Tyler Walters <tyler@tylerwalters.com>
2016-07-22 13:26:57 -0700

    Converting to ES6 style, adding babel task

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,66 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const jshint = require('gulp-jshint');
+const minifycss = require('gulp-minify-css');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
-gulp.task('lint', function () {
-  var jshint = require('gulp-jshint');
-
+gulp.task('lint', () => {
   return gulp.src(['src/scripts/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('sass', function () {
-  var sass = require('gulp-sass'),
-      sourcemaps = require('gulp-sourcemaps');
-
+gulp.task('sass', () => {
   return gulp.src('src/styles/style.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('css-min', function () {
-  var minifycss = require('gulp-minify-css'),
-      concat    = require('gulp-concat');
-
+gulp.task('css-min', () => {
   return gulp.src('dist/styles/style.css')
     .pipe(minifycss())
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('js', function () {
-  var concat = require('gulp-concat'),
-      sourcemaps = require('gulp-sourcemaps');
-
+gulp.task('babel', () => {
   return gulp.src('src/scripts/app.js')
     .pipe(sourcemaps.init())
-    .pipe(concat('app.js'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('js-min', function () {
-  var uglify = require('gulp-uglify'),
-      concat = require('gulp-concat');
-
+gulp.task('js-min', () => {
   return gulp.src('dist/scripts/app.js')
     .pipe(uglify())
     .pipe(concat('app.min.js'))
     .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch('src/styles/**/*.scss', ['sass']);
-  gulp.watch('src/scripts/**/*.js', ['js']);
+gulp.task('templates', () => {
+  return gulp.src('src/templates/**/*.html')
+    .pipe(gulp.dest('./dist/templates'));
 });
 
-gulp.task('minify', ['css-min', 'js-min']);
-gulp.task('build', ['lint', 'sass', 'js', 'css-min', 'js-min']);
+gulp.task('imagemin', () => {
+  return gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/styles/**/*.scss', ['sass']);
+  gulp.watch('src/scripts/**/*.js', ['babel']);
+  gulp.watch('src/templates/**/*.html', ['templates']);
+  gulp.watch('src/images/*', ['imagemin']);
+});
+
+gulp.task('minify', ['css-min', 'js-min', 'imagemin']);
+gulp.task('build', ['lint', 'sass', 'babel', 'templates', 'imagemin', 'css-min', 'js-min']);
