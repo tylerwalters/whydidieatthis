9937d737342a7640e276c5ab4d4f9ec5f0678e55
Tyler Walters <tyler@tylerwalters.com>
2016-07-22 11:06:05 -0700

    Adding stylesheet, updating custom post type to work with REST API

--- functions.php
+++ functions.php
@@ -1,25 +1,52 @@
 <?php
 
 function enqueue_scripts() {
-  wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/node_modules/bootstrap/dist/3.3.5/css/bootstrap.min.css');
+  wp_enqueue_style('style-css', get_template_directory_uri() . '/dist/styles/style.css');
   wp_enqueue_script('angular-core', get_template_directory_uri() . '/node_modules/angular/angular.min.js', array('jquery'), false, true);
   wp_enqueue_script('angular-resource', get_template_directory_uri() . '/node_modules/angular-resource/angular-resource.min.js', array('angular-core'), false, true);
   wp_enqueue_script('ui-router', get_template_directory_uri() . '/node_modules/angular-ui-router/release/angular-ui-router.min.js', array('angular-core'), false, true);
-  wp_enqueue_script('main-script', get_template_directory_uri() . '/dist/js/main.js', array('ui-router'), false, true);
-  wp_localize_script('main-script', 'constants', array(
-      'api-url' => rest_get_url_prefix() . '/wp/v2/',
-      'template-directory' => get_template_directory_uri() . '/'
+  wp_enqueue_script('app-script', get_template_directory_uri() . '/dist/scripts/app.js', array('ui-router'), false, true);
+  wp_localize_script('app-script', 'constants', array(
+      'apiUrl' => rest_get_url_prefix() . '/wp/v2/',
+      'templateDir' => get_template_directory_uri() . '/'
     )
   );
 }
 
 function reviews_cpt() {
+  $labels = array(
+    'name'               => _x('Reviews', 'post type general name'),
+    'singular_name'      => _x('Review', 'post type singular name'),
+    'menu_name'          => _x('Reviews', 'admin menu'),
+    'name_admin_bar'     => _x('Review', 'add new on admin bar'),
+    'add_new'            => _x('Add New', 'reviews'),
+    'add_new_item'       => __('Add New Review'),
+    'new_item'           => __('New Review'),
+    'edit_item'          => __('Edit Review'),
+    'view_item'          => __('View Review'),
+    'all_items'          => __('All Reviews'),
+    'search_items'       => __('Search Reviews'),
+    'parent_item_colon'  => __('Parent Reviews:'),
+    'not_found'          => __('No reviews found.'),
+    'not_found_in_trash' => __('No reviews found in Trash.')
+  );
+
   $args = array(
-    'labels'  => array(
-      'name' => 'Reviews',
-      'singular_name' => 'Review'
-    ),
-    'public' => true
+    'labels'             => $labels,
+    'public'             => true,
+    'publicly_queryable' => true,
+    'show_ui'            => true,
+    'show_in_menu'       => true,
+    'query_var'          => true,
+    'rewrite'            => array('slug' => 'reviews'),
+    'capability_type'    => 'post',
+    'has_archive'        => true,
+    'hierarchical'       => false,
+    'menu_position'      => null,
+    'show_in_rest'       => true,
+    'rest_base'          => 'reviews',
+    'rest_controller_class' => 'WP_REST_Posts_Controller',
+    'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
   );
 
   register_post_type('reviews', $args);
