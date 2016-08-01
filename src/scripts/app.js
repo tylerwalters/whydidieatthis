961915c1be31a01314076f0d8b0238a125096c47
Tyler Walters <tyler@tylerwalters.com>
2016-07-31 18:13:35 -0700

    Refactoring to use single posts endpoint after removing custom post type, adding additional route

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,43 +4,30 @@ app.factory('Store', ['$resource', '$rootScope', function ($resource, $rootScope
   let Store = {};
   let endpoints = {};
 
-  endpoints.features = $resource(constants.apiUrl + 'posts/:ID', {ID: '@id'});
-  endpoints.reviews = $resource(constants.apiUrl + 'reviews/:ID', {ID: '@id'});
+  endpoints.posts = $resource(constants.apiUrl + 'posts/:ID', {ID: '@id'});
 
   Store.posts = [];
   Store.features = [];
   Store.reviews = [];
 
-  Store.fetchFeatures = () => {
-    endpoints.features.query((res => {
-      Store.features = res;
-      Store.posts = Store.combinePosts();
-      $rootScope.$emit('fetchFeatures', res);
+  Store.fetchPosts = () => {
+    endpoints.posts.query((res => {
+      Store.posts = res;
+      $rootScope.$emit('fetchPosts', res);
     }));
   };
 
-  Store.fetchReviews = () => {
-    endpoints.reviews.query((res => {
-      Store.reviews = res;
-      Store.posts = Store.combinePosts();
-      $rootScope.$emit('fetchReviews', res);
-    }));
-  };
-
-  Store.fetchSingle = (type, name) => {
-    endpoints[type].query({'filter[name]': name}, (res) => {
+  Store.fetchSingle = (name) => {
+    endpoints.posts.query({'filter[name]': name}, (res) => {
       Store.single = res[0];
       $rootScope.$emit('fetchSingle', res);
     });
   };
 
-  Store.combinePosts = () => [].concat(Store.features, Store.reviews);
   Store.getPosts = () => Store.posts;
-  Store.sortPosts = (posts) => posts.sort((a, b) => new Date(b.date) - new Date(a.date));
   Store.getSingle = () => Store.single;
 
-  // Store.fetchFeatures();
-  Store.fetchReviews();
+  Store.fetchPosts();
 
   return Store;
 }]);
@@ -64,14 +51,10 @@ app.component('logo', {
 function ListController($scope, $element, $attrs, Store, $rootScope) {
   let ctrl = this;
 
-  ctrl.list = Store.sortPosts(Store.getPosts());
+  ctrl.list = Store.getPosts();
 
-  $rootScope.$on('fetchFeatures', (evt, arg) => {
-    ctrl.list = Store.sortPosts(Store.getPosts());
-  });
-
-  $rootScope.$on('fetchReviews', (evt, arg) => {
-    ctrl.list = Store.sortPosts(Store.getPosts());
+  $rootScope.$on('fetchPosts', (evt, arg) => {
+    ctrl.list = Store.getPosts();
   });
 }
 
@@ -106,7 +89,7 @@ app.component('listPost', {
 function DetailController($scope, $element, $attrs, $stateParams, $rootScope, Store) {
   let ctrl = this;
 
-  Store.fetchSingle($stateParams.type, $stateParams.name);
+  Store.fetchSingle($stateParams.name);
   ctrl.post = Store.getSingle();
 
   $rootScope.$on('fetchSingle', (evt, arg) => {
@@ -119,16 +102,22 @@ app.component('detail', {
   controller: DetailController
 });
 
-app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvider, $locationProvider) {
   $locationProvider .html5Mode(true);
   $urlRouterProvider.otherwise('/');
+  $urlMatcherFactoryProvider.strictMode(false);
+  
   $stateProvider
     .state('list', {
       url: '/',
       templateUrl: constants.templateDir + 'dist/templates/home.html'
     })
+    .state('about', {
+      url: '/about',
+      templateUrl: constants.templateDir + 'dist/templates/about.html'
+    })
     .state('detail', {
-      url: '/:type/:name',
+      url: '/:name',
       templateUrl: constants.templateDir + 'dist/templates/single.html'
     });
 });
