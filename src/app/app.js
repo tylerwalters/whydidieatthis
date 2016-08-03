b786187f1285d9fd4402c1c4e7a64ce542dbaf6a
Tyler Walters <tyler@tylerwalters.com>
2016-08-02 17:19:10 -0700

    Adding single view module, adding logo component

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,10 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './app.config';
 import home from './views/home';
+import single from './views/single';
+import logo from './components/logo';
 
-angular.module('app', [uiRouter, home])
+angular.module('app', [uiRouter, home, single])
   .config(routing)
+  .component('logo', logo)
   .filter('trustAsHTML', ['$sce', $sce => text => $sce.trustAsHtml(text)]);
