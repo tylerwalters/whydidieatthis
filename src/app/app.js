a529771ec19c2b8754de1b185899582389ac46c7
Tyler Walters <tyler@tylerwalters.com>
2016-08-02 17:46:43 -0700

    Moving loader component to main app file, adding loader to home page

--- src/app/app.js
+++ src/app/app.js
@@ -5,8 +5,10 @@ import routing from './app.config';
 import home from './views/home';
 import single from './views/single';
 import logo from './components/logo';
+import loader from './components/loader';
 
 angular.module('app', [uiRouter, home, single])
   .config(routing)
   .component('logo', logo)
+  .component('loader', loader)
   .filter('trustAsHTML', ['$sce', $sce => text => $sce.trustAsHtml(text)]);
--- src/app/views/single/index.js
+++ src/app/views/single/index.js
@@ -4,13 +4,11 @@ import ngResource from 'angular-resource';
 import routing from './single.routes.js';
 import SingleController from './single.controller.js';
 import detail from '../../components/detail';
-import loader from '../../components/loader';
 import PostService from '../../services/post.service';
 
 export default angular.module('app.single', [uiRouter, ngResource])
   .config(routing)
   .controller('SingleController', SingleController)
   .component('detail', detail)
-  .component('loader', loader)
   .service('postService', PostService)
   .name;
