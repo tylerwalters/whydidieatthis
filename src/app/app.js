4f5bfe07d52e98677093b1d205ad23ab0c164cea
Tyler Walters <tyler@tylerwalters.com>
2016-08-07 22:50:31 -0700

    Importing page view

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,12 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './app.config';
 import home from './views/home';
+import page from './views/page';
 import single from './views/single';
 import logo from './components/logo.component';
 import loader from './components/loader.component';
 
-angular.module('app', [uiRouter, home, single])
+angular.module('app', [uiRouter, home, single, page])
   .config(routing)
   .component('logo', logo)
   .component('loader', loader)
