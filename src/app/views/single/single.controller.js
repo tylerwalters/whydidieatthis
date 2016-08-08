16ef2a9a5eb04c31ff0271dbdb89a7878343c8a3
Tyler Walters <tyler@tylerwalters.com>
2016-08-07 23:01:06 -0700

    Retrieving post with id if available

--- src/app/views/single/single.controller.js
+++ src/app/views/single/single.controller.js
@@ -2,13 +2,21 @@ export default class SingleController {
   constructor (postService, $stateParams) {
     'ngInject';
     this.postService = postService;
+    this.id = $stateParams.id;
     this.name = $stateParams.name;
   }
 
   $onInit () {
-    this.postService.query({'filter[name]': this.name}).$promise
-      .then(res => {
-        this.post = res[0];
-      });
+    if (this.id) {
+      this.postService.get({ID: this.id}).$promise
+        .then(res => {
+          this.post = res;
+        });
+    } else {
+      this.postService.query({'filter[name]': this.name}).$promise
+        .then(res => {
+          this.post = res[0];
+        });
+    }
   }
 }
