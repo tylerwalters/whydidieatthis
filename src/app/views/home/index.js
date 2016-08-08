31d27d324a6c38c290084ecf950c44626da44852
Tyler Walters <tyler@tylerwalters.com>
2016-08-07 23:02:59 -0700

    Removing imports for components that are no longer used

--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -3,14 +3,12 @@ import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
 import routing from './home.routes.js';
 import HomeController from './home.controller.js';
-import list from './list.component';
 import post from './post.component';
 import PostService from '../../services/post.service';
 
 export default angular.module('app.home', [uiRouter, ngResource])
   .config(routing)
   .controller('HomeController', HomeController)
-  .component('list', list)
   .component('post', post)
   .service('postService', PostService)
   .name;
--- src/app/views/single/index.js
+++ src/app/views/single/index.js
@@ -3,12 +3,10 @@ import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
 import routing from './single.routes.js';
 import SingleController from './single.controller.js';
-import detail from './detail.component';
 import PostService from '../../services/post.service';
 
 export default angular.module('app.single', [uiRouter, ngResource])
   .config(routing)
   .controller('SingleController', SingleController)
-  .component('detail', detail)
   .service('postService', PostService)
   .name;
