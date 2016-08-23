e08cd6b9a87a6a7d1018054c9bd5ca09cbbe8a24
Tyler Walters <tyler@tylerwalters.com>
2016-08-22 17:51:13 -0700

    Adding PageService dependency

--- src/app/services/state/index.js
+++ src/app/services/state/index.js
@@ -2,10 +2,12 @@ import angular from 'angular';
 import ngResource from 'angular-resource';
 import StateService from './state.service';
 import PostService from './post.service.js';
+import PageService from './page.service.js';
 import CategoryService from './category.service.js';
 
 export default angular.module('app.state', [ngResource])
   .service('stateService', StateService)
   .service('postService', PostService)
+  .service('pageService', PageService)
   .service('categoryService', CategoryService)
   .name;
