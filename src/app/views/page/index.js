d693ba91ca62131b7e737129b7f1079418db2cf3
Tyler Walters <tyler@tylerwalters.com>
2016-08-22 17:50:59 -0700

    Removing PageService import

--- src/app/views/page/index.js
+++ src/app/views/page/index.js
@@ -3,7 +3,6 @@ import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
 import routing from './page.routes.js';
 import PageController from './page.controller.js';
-import PageService from '../../services/state/page.service.js';
 
 export default angular.module('app.page', [uiRouter, ngResource])
   .config(routing)
