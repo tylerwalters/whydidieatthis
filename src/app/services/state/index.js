c6baf012341860b2aefa42a1a12f7911efd1d1f2
Tyler Walters <tyler@tylerwalters.com>
2016-08-23 01:24:29 -0700

    Adding AuthorService and CommentService

--- src/app/services/state/index.js
+++ src/app/services/state/index.js
@@ -1,13 +1,17 @@
 import angular from 'angular';
 import ngResource from 'angular-resource';
 import StateService from './state.service';
-import PostService from './post.service.js';
-import PageService from './page.service.js';
-import CategoryService from './category.service.js';
+import PostService from './post.service';
+import PageService from './page.service';
+import CategoryService from './category.service';
+import AuthorService from './author.service';
+import CommentService from './comment.service';
 
 export default angular.module('app.state', [ngResource])
   .service('stateService', StateService)
   .service('postService', PostService)
   .service('pageService', PageService)
   .service('categoryService', CategoryService)
+  .service('authorService', AuthorService)
+  .service('commentService', CommentService)
   .name;
