2d5e636ce0ad2288a00c66f16d67f65455b6caf8
Tyler Walters <tyler@tylerwalters.com>
2016-08-22 17:09:34 -0700

    Removing individual services that were moved into StateService

--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -4,11 +4,9 @@ import ngResource from 'angular-resource';
 import routing from './home.routes.js';
 import HomeController from './home.controller.js';
 import post from './post.component';
-import PostService from '../../services/post.service';
 
 export default angular.module('app.home', [uiRouter, ngResource])
   .config(routing)
   .controller('HomeController', HomeController)
   .component('post', post)
-  .service('postService', PostService)
   .name;
--- src/app/views/single/index.js
+++ src/app/views/single/index.js
@@ -3,10 +3,8 @@ import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
 import routing from './single.routes.js';
 import SingleController from './single.controller.js';
-import PostService from '../../services/post.service';
 
 export default angular.module('app.single', [uiRouter, ngResource])
   .config(routing)
   .controller('SingleController', SingleController)
-  .service('postService', PostService)
   .name;
