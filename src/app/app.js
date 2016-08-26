9a4a422939dfbaf22d5b19bca055a841d4e7c97c
Tyler Walters <tyler@tylerwalters.com>
2016-08-25 19:33:21 -0700

    Removing state import

--- src/app/app.js
+++ src/app/app.js
@@ -2,14 +2,13 @@ import '../styles/style.scss';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './app.config';
-import state from './services/state';
 import home from './views/home';
 import page from './views/page';
 import single from './views/single';
 import logo from './components/logo.component';
 import loader from './components/loader.component';
 
-angular.module('app', [uiRouter, state, home, page, single])
+angular.module('app', [uiRouter, home, page, single])
   .config(routing)
   .component('logo', logo)
   .component('loader', loader)
