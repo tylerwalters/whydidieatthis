27023a074e731fbd82315a734ab9ea9e496878a6
Tyler Walters <tyler@tylerwalters.com>
2016-08-25 19:26:25 -0700

    Adding services to modules

--- src/app/views/home/index.js
+++ src/app/views/home/index.js
@@ -1,12 +1,16 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
-import routing from './home.routes.js';
-import HomeController from './home.controller.js';
+import routing from './home.routes';
+import HomeController from './home.controller';
+import PostService from '../../services/post.service';
+import CategoryService from '../../services/category.service';
 import post from './post.component';
 
 export default angular.module('app.home', [uiRouter, ngResource])
   .config(routing)
   .controller('HomeController', HomeController)
+  .service('postService', PostService)
+  .service('categoryService', CategoryService)
   .component('post', post)
   .name;
--- src/app/views/page/index.js
+++ src/app/views/page/index.js
@@ -1,9 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
-import routing from './page.routes.js';
-import PageController from './page.controller.js';
-import PageService from '../../services/state/page.service.js';
+import routing from './page.routes';
+import PageController from './page.controller';
+import PageService from '../../services/page.service';
 
 export default angular.module('app.page', [uiRouter, ngResource])
   .config(routing)
