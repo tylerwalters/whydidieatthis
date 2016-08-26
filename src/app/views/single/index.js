b3dfe4436651e24dac20eefad7795637da5f9bde
Tyler Walters <tyler@tylerwalters.com>
2016-08-25 19:26:46 -0700

    Importing service

--- src/app/views/single/index.js
+++ src/app/views/single/index.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
-import routing from './single.routes.js';
-import SingleController from './single.controller.js';
+import routing from './single.routes';
+import SingleController from './single.controller';
+import PostService from '../../services/post.service';
 
 export default angular.module('app.single', [uiRouter, ngResource])
   .config(routing)
   .controller('SingleController', SingleController)
+  .service('postService', PostService)
   .name;
