ecc2ae3f481843b51f796b95c4f30bc6a318fcf3
Tyler Walters <tyler@tylerwalters.com>
2016-08-26 10:24:20 -0700

    Dynamically setting page title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title><?php bloginfo('name'); ?></title>
+  <title ng-bind-html="pageTitle | pagetitle | trustAsHTML">Phoenix Wings and Beer - Why Did I Eat This?</title>
 
   <?php wp_head(); ?>
 
